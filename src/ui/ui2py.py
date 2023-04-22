fcfab8bc0bcde750814374d0a3d813965158df13
Daniel <mutex_@outlook.com>
2023-04-22 11:53:34 +0800

    编写脚本能够自动监视ui文件的修改使其能够自动生成py文件

---
+++ src/ui/ui2py.py
@@ -0,0 +1,36 @@
+import sys, os, time
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import subprocess
+
+class MyHandler(FileSystemEventHandler):
+    # def on_modified(self, event):
+    #     if event.src_path.endswith('.ui'):
+    #         print(f'{event.src_path} has been modified, updating .py file')
+    #         filename, file_extension = os.path.splitext(os.path.basename(event.src_path))
+    #         output_filename = 'ui_' + filename + '.py'
+    #         output_path = os.path.join(os.path.dirname(event.src_path), output_filename)
+    #         subprocess.run(['pyuic5.exe', event.src_path, '-o', output_path])
+
+    def on_moved(self, event):
+        if event.dest_path.endswith('.ui'):
+            print(f'{event.dest_path} has been moved, updating .py file')
+            filename, file_extension = os.path.splitext(os.path.basename(event.dest_path))
+            output_filename = 'ui_' + filename + '.py'
+            output_path = os.path.join(os.path.dirname(event.dest_path), output_filename)
+            subprocess.run(['pyuic5.exe', event.dest_path, '-o', output_path])
+
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
