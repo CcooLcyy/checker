36f5531cb26cd1641a351c15fba68533964a5d8e
Daniel <mutex_@outlook.com>
2023-05-06 01:08:09 +0800

    修改格式，增加数据库底层删除逻辑；增加修改密码逻辑；增加查询所有用户逻辑；增加删除用户逻辑

--- src/func/mysql.py
+++ src/func/mysql.py
@@ -1,5 +1,5 @@
 import pymysql, zipfile, hashlib
-from file import File
+from .file import File
 
 class Mysql():
     def __init__(self):
@@ -42,39 +42,46 @@ class Mysql():
         self.cursor.execute(insert, values)
         self.connection.commit()
 
-    def __query(self, table_name, conditions=None, *column_names):
-        columns_str = ', '.join(column_names)
+    def __query(self, tableName, conditions=None, *column_names):
+        columnsStr = ', '.join(column_names)
         if conditions:
-            conditions_str = ' AND '.join([f"{key} = %s" for key in conditions.keys()])
-            sql = f"SELECT {columns_str} FROM {table_name} WHERE {conditions_str}"
+            conditionsStr = ' AND '.join([f"{key} = %s" for key in conditions.keys()])
+            sql = f"SELECT {columnsStr} FROM {tableName} WHERE {conditionsStr}"
             values = tuple(conditions.values())
             self.cursor.execute(sql, values)
         else:
-            sql = f"SELECT {columns_str} FROM {table_name}"
+            sql = f"SELECT {columnsStr} FROM {tableName}"
             self.cursor.execute(sql)
         result = self.cursor.fetchall()
         return result
 
-    def __createTable(self, table_name, columns):
-        columns_str = ', '.join([f"{column_name} {column_type}" for column_name, column_type in columns.items()])
-        sql = f"CREATE TABLE {table_name} ({columns_str})"
+    def __createTable(self, tableName, columns):
+        columnsStr = ', '.join([f"{columnName} {columnType}" for columnName, columnType in columns.items()])
+        sql = f"CREATE TABLE {tableName} ({columnsStr})"
         self.cursor.execute(sql)
         self.connection.commit()
     
-    def __update(self, table_name, data, condition):
-        set_values = ', '.join([f"{key} = %s" for key in data.keys()])
-        condition_str = ' AND '.join([f"{key} = %s" for key in condition.keys()])
-        sql = f"UPDATE {table_name} SET {set_values} WHERE {condition_str}"
+    def __update(self, tableName, data, condition):
+        setValues = ', '.join([f"{key} = %s" for key in data.keys()])
+        conditionStr = ' AND '.join([f"{key} = %s" for key in condition.keys()])
+        sql = f"UPDATE {tableName} SET {setValues} WHERE {conditionStr}"
         values = list(data.values()) + list(condition.values())
         self.cursor.execute(sql, values)
         self.connection.commit()
 
-    def md5_encrypt(self, text):
+    def __delete(self, tableName: str, condition: map) -> None:
+        conditionStr = ' AND '.join([f"{key} = %s" for key in condition.keys()])
+        sql = f"DELETE FROM {tableName} WHERE {conditionStr}"
+        value = tuple(condition.values())
+        self.cursor.execute(sql, value)
+        self.connection.commit()
+
+    def md5_encrypt(self, text: str) -> str:
         msg = hashlib.md5()
         msg.update(text.encode('utf-8'))
         return msg.hexdigest()
 
-    def userExist(self, userName):
+    def userExist(self, userName: str):
         result = self.__query('user', {'user_name': f'{userName}'}, 'user_name')
         if len(result) == 0:
             return False
@@ -89,17 +96,32 @@ class Mysql():
     def addUser(self, userName, password):
         passwordMd5 = self.md5_encrypt(password)
         if self.__query('user', {'user_name': f'{userName}', 'user_password': f'{passwordMd5}'}, 'user_name'):
-            print('用户存在')
+            return '用户存在'
         else:
             self.__insert('user', {'user_name': f'{userName}', 'user_password': f'{passwordMd5}'})
+            return f'用户：{userName}添加成功，密码为：{password}'
         
-    def password(self, userName, password):
-        passwordMd5 = self.md5_encrypt(password)
-        self.__update('user', {'user_password': f'{passwordMd5}'}, {'user_name':f'{userName}'})
+    def changePassword(self, userName, password):
+        if not self.userExist(userName):
+            return '用户不存在，请先添加用户'
+        else:
+            passwordMd5 = self.md5_encrypt(password)
+            self.__update('user', {'user_password': f'{passwordMd5}'}, {'user_name': f'{userName}'})
+            return f'用户：{userName}密码已修改为：{password}'
     
+    def queryAllUser(self):
+        result = self.__query('user', None, 'user_name')
+        return result
+
+    def deleteUser(self, userName: str) -> None:
+        self.__delete('user', {'user_name': f'{userName}'})
+
+
     def test(self):
         pass
 
 if __name__ == "__main__":
     sql = Mysql()
-    sql.addUser('test', 'test')
+    # print(len(sql.queryAllUser()))
+    # for _ in sql.queryAllUser():
+    #     print(f'{_[0]}')
