b664a67a757c711458d13f0249a1227642e163a7
Daniel <mutex_@outlook.com>
2023-05-08 19:30:30 +0800

    添加子查询的内容

--- src/func/mysql.py
+++ src/func/mysql.py
@@ -1,9 +1,10 @@
 import pymysql, zipfile, hashlib
-from file import File
+from .file import File
 
 class Mysql():
     def __init__(self):
         self.file = File()
+        self.user_table = 'users'
         try:
             self.connection = pymysql.connect(
                 host = 'localhost',
@@ -15,13 +16,13 @@ class Mysql():
             if self.connection != None:
                 self.cursor = self.connection.cursor()
             if not self.userExist('admin'):
-                self.__insert('user', {'user_id': '1', 'user_name': 'admin', 'user_password': f"{self.md5_encrypt('admin')}"})
+                self.__insert(self.user_table, {'user_id': '1', 'user_name': 'admin', 'user_password': f"{self.md5_encrypt('admin')}"})
                 self.__init__()
         except Exception as e:
             if e.args[0] == 2003:
                 self.installed = False
             elif e.args[0] == 1146:
-                self.__createTable('user', {'user_id': 'INT(32) AUTO_INCREMENT PRIMARY KEY', 'user_name': 'CHAR(32)', 'user_password': 'CHAR(32)'})
+                self.__createTable(self.user_table, {'user_id': 'INT(32) AUTO_INCREMENT PRIMARY KEY', 'user_name': 'CHAR(32)', 'user_password': 'CHAR(32)'})
             elif e.args[0] == 1049:
                 self.connection = pymysql.connect(
                     host='localhost',
@@ -34,7 +35,6 @@ class Mysql():
             else:
                 print(e)
             self.__init__()
-
 
     def __del__(self):
         self.cursor.close()
@@ -66,6 +66,19 @@ class Mysql():
             self.cursor.execute(sql)
         result = self.cursor.fetchall()
         return result
+    
+    def __subQuery(self, tableName, subTableName, conditions=None, subConditions=None, *subColumnNames):
+        subQueryResult = ', '.join([str(x[0]) for x in self.__query(subTableName, subConditions, *subColumnNames)])
+        if conditions:
+            conditionsStr = ' AND '.join([f"{key} = %s" for key in conditions.keys()])
+            sql = f"SELECT * FROM {tableName} WHERE {conditionsStr} AND {subColumnNames[0]} IN ({subQueryResult})"
+            values = tuple(conditions.values())
+            self.cursor.execute(sql, values)
+        else:
+            sql = f"SELECT * FROM {tableName} WHERE {subColumnNames[0]} IN ({subQueryResult})"
+            self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+        return result
 
     def __createTable(self, tableName, columns):
         columnsStr = ', '.join([f"{columnName} {columnType}" for columnName, columnType in columns.items()])
@@ -94,23 +107,23 @@ class Mysql():
         return msg.hexdigest()
 
     def userExist(self, userName: str):
-        result = self.__query('user', {'user_name': f'{userName}'}, 'user_name')
+        result = self.__query(self.user_table, {'user_name': f'{userName}'}, 'user_name')
         if len(result) == 0:
             return False
         elif result[0][0] == userName:
             return True
         
     def verifyPassword(self, userName):
-        result = self.__query('user', {'user_name': f'{userName}'}, 'user_password')
+        result = self.__query(self.user_table, {'user_name': f'{userName}'}, 'user_password')
         if len(result) != 0:
             return result[0][0]
 
     def addUser(self, userName, password):
         passwordMd5 = self.md5_encrypt(password)
-        if self.__query('user', {'user_name': f'{userName}', 'user_password': f'{passwordMd5}'}, 'user_name'):
+        if self.__query(self.user_table, {'user_name': f'{userName}', 'user_password': f'{passwordMd5}'}, 'user_name'):
             return '用户存在'
         else:
-            self.__insert('user', {'user_name': f'{userName}', 'user_password': f'{passwordMd5}'})
+            self.__insert(self.user_table, {'user_name': f'{userName}', 'user_password': f'{passwordMd5}'})
             return f'用户：{userName}添加成功，密码为：{password}'
         
     def changePassword(self, userName, password):
@@ -118,15 +131,15 @@ class Mysql():
             return '用户不存在，请先添加用户'
         else:
             passwordMd5 = self.md5_encrypt(password)
-            self.__update('user', {'user_password': f'{passwordMd5}'}, {'user_name': f'{userName}'})
+            self.__update(self.user_table, {'user_password': f'{passwordMd5}'}, {'user_name': f'{userName}'})
             return f'用户：{userName}密码已修改为：{password}'
     
     def queryAllUser(self):
-        result = self.__query('user', None, 'user_name')
+        result = self.__query(self.user_table, None, 'user_name')
         return result
 
     def deleteUser(self, userName: str) -> None:
-        self.__delete('user', {'user_name': f'{userName}'})
+        self.__delete(self.user_table, {'user_name': f'{userName}'})
 
 
     def test(self):
@@ -134,6 +147,3 @@ class Mysql():
 
 if __name__ == "__main__":
     sql = Mysql()
-    # print(len(sql.queryAllUser()))
-    # for _ in sql.queryAllUser():
-    #     print(f'{_[0]}')
