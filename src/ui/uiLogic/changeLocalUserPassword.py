c0d3e5bdefedcd9a3de717d760999c0c78f6f5e6
Daniel <mutex_@outlook.com>
2023-05-14 23:03:17 +0800

    添加修改业务用户密码的功能

---
+++ src/ui/uiLogic/changeLocalUserPassword.py
@@ -0,0 +1,29 @@
+from PyQt5 import QtWidgets
+from ui.ui_changeLocalUserPasswordWindow import Ui_changeLocalUserPassword
+import sys
+sys.path.append('src')
+from func.mysql import Mysql
+
+class ChangeLocalUserPassword(QtWidgets.QWidget, Ui_changeLocalUserPassword):
+    def __init__(self, userName):
+        super().__init__()
+        self.setupUi(self)
+
+        self.sql = Mysql()
+        self.userName = userName
+
+        self.userNameShow.setText(self.userName)
+
+        self.changePasswordButton.clicked.connect(self.changePasswordSlot)
+
+    def changePasswordSlot(self):
+        oldPasswordMd5 = self.sql.md5_encrypt(self.oldPassword.text())
+        if oldPasswordMd5 == self.sql.verifyPassword(self.userName):
+            if self.sql.md5_encrypt(self.newPassword.text()) == self.sql.md5_encrypt(self.newPasswordRe.text()):
+                self.sql.changePassword(self.userName, self.newPassword.text())
+                QtWidgets.QMessageBox.information(self, '提示', '密码修改成功')
+                self.close()
+            else:
+                QtWidgets.QMessageBox.information(self, '提示', '两次密码输入不一致')
+        else:
+            QtWidgets.QMessageBox.information(self, '提示', '原密码输入错误')
